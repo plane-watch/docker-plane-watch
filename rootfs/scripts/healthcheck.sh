32ba54b234e402294780adb4d8d06e88908560ee
Mike Nye <mike.nye@gmail.com>
2026-04-13 10:06:37 +0800

    fix mlat endpoint

--- rootfs/scripts/healthcheck.sh
+++ rootfs/scripts/healthcheck.sh
@@ -6,9 +6,10 @@ EXITCODE=0
 
 BESTHOST_RESOLVED=$(getent hosts "$BEASTHOST" | tr -s " " | cut -d " " -f 1)
 PW_BEAST_ENDPOINT_RESOLVED=$(getent hosts "$PW_BEAST_ENDPOINT" | tr -s " " | cut -d " " -f 1)
+PW_MLAT_ENDPOINT_RESOLVED=$(getent hosts "$PW_MLAT_ENDPOINT" | tr -s " " | cut -d " " -f 1)
 
 # check pw-feeder to beasthost connection
-echo -n "pw-feeder connected to $BEASTHOST:$BEASTPORT: "
+echo -n "pw-feeder connected to $BEASTHOST:$BEASTPORT ($BESTHOST_RESOLVED:$BEASTPORT): "
 if ! ss --tcp --processes state established dst "$BESTHOST_RESOLVED" \&\& dport "$BEASTPORT" 2>/dev/null| grep -q pw-feeder; then
     EXITCODE=1
     echo "FAIL"
@@ -17,7 +18,7 @@ else
 fi
 
 # check pw-feeder to plane.watch BEAST connection
-echo -n "pw-feeder connected to $PW_BEAST_ENDPOINT: "
+echo -n "pw-feeder connected to $PW_BEAST_ENDPOINT ($PW_BEAST_ENDPOINT_RESOLVED): "
 if ! ss --tcp --processes state established dst "$PW_BEAST_ENDPOINT_RESOLVED" 2>/dev/null| grep -q pw-feeder; then
     EXITCODE=1
     echo "FAIL"
@@ -29,7 +30,7 @@ fi
 if [[ "${ENABLE_MLAT,,}" == "true" ]]; then
 
     # check mlat-client to beasthost connection
-    echo -n "mlat-client connected to $BEASTHOST:$BEASTPORT: "
+    echo -n "mlat-client connected to $BEASTHOST:$BEASTPORT ($BESTHOST_RESOLVED:$BEASTPORT): "
     if ! ss --tcp --processes state established dst "$BESTHOST_RESOLVED" \&\& dport "$BEASTPORT" 2>/dev/null | grep -q mlat-client; then
         EXITCODE=1
         echo "FAIL"
@@ -56,8 +57,8 @@ if [[ "${ENABLE_MLAT,,}" == "true" ]]; then
     fi
 
     # check pw-feeder to plane.watch MLAT connection
-    echo -n "pw-feeder connected to $PW_MLAT_ENDPOINT: "
-    if ! ss --tcp --processes state established dst "$PW_BEAST_ENDPOINT_RESOLVED" 2>/dev/null| grep -q pw-feeder; then
+    echo -n "pw-feeder connected to $PW_MLAT_ENDPOINT ($PW_MLAT_ENDPOINT_RESOLVED): "
+    if ! ss --tcp --processes state established dst "$PW_MLAT_ENDPOINT_RESOLVED" 2>/dev/null| grep -q pw-feeder; then
         EXITCODE=1
         echo "FAIL"
     else
